0e9445adda4c40dd5bbdb4d516e158c84d4d7961
vagrant <vagrant@localhost.localdomain>
2020-04-10 10:02:19 +0000

    [Finish] lesson7 describe, contextを使おう

--- spec/calc_spec.rb
+++ spec/calc_spec.rb
@@ -1,14 +1,14 @@
 require 'calc'
 
-RSpec.describe "A calc" do
-  before(:example) do 
-    @calc = Calc.new
+RSpec.describe Calc do
+  context "when normal mode" do
+    it "givven 2 and 3, returns 5" do
+      calc = Calc.new
+      expect(calc.add(2, 3)).to eq(5)
+    end
   end
-
-  it "given 2 and 3, returns 5" do
-    expect(@calc.add(2, 3)).to eq(5)
-  end
-  it "given 5 and 8, returns 13" do
-    expect(@calc.add(5, 8)).to eq(13)
+  context "when graph mode" do
+    it "draws graph" do
+    end
   end
 end
